c94a9b43a615fbdd5734e6fac5c6db04f58d0381
sadad64 <sadad64@naver.com>
2022-08-10 22:25:17 +0900

    [add] image api test

--- src/app/Contents/contentsController.js
+++ src/app/Contents/contentsController.js
@@ -2,6 +2,7 @@ const contentsProvider = require("../../app/Contents/contentsProvider");
 const contentsService = require("../../app/Contents/contentsService");
 const baseResponse = require("../../../config/baseResponseStatus");
 const {response, errResponse} = require("../../../config/response");
+const multer = require("multer");
 
 /**
  * API No. 4.1
@@ -23,4 +24,21 @@ const {response, errResponse} = require("../../../config/response");
  exports.getRecommendBooks = async function(req, res){
     const RecommendBooksResult = await contentsProvider.retrieveRecommendBooks();
     return res.send(response(baseResponse.SUCCESS, RecommendBooksResult));
+}
+
+/**
+ * API No. 4.3
+ * API Name : 프로필 이미지 업로드 API
+ * [POST] /contents/uploadProfileImg/',
+ * 
+ */
+ exports.uploadProfileImg = async function(req, res){
+
+    console.log(req.file);
+    console.log(req.body);
+
+    
+
+    // const RecommendBooksResult = await contentsProvider.retrieveRecommendBooks();
+    return res.send(req.file);
 }
--- src/app/Contents/contentsRoute.js
+++ src/app/Contents/contentsRoute.js
@@ -1,10 +1,38 @@
 module.exports = function(app){
     const contents = require('./contentsController');
+    const multer = require("multer");
 
+    // 확장자 알아내는 함수
+    function getExt(fileName) {
+        if (fileName.indexOf(".") == -1) {
+          let fileExt = "Directory";
+          return fileExt;
+        }
+    
+        let fileLength = fileName.length;
+        let lastDot = fileName.lastIndexOf(".");
+        let fileExt = fileName.substring(lastDot, fileLength).toLowerCase().replace(".", "");
+    
+        return fileExt;
+      }
 
     // 4.1 공지사항 배너 조회 API
     app.get('/contents/notice/', contents.getNotice);
 
     // 4.2 추천 책 조회 API
     app.get('/contents/recommendBooks/', contents.getRecommendBooks);
+
+    // 4.3 프로필 이미지 업로드 API
+    const storage = multer.diskStorage({
+        destination: '/home/ubuntu/source/profileImg',
+        filename: (req, file, cb) => {
+            return cb (null, `${file.fieldname}_${Date.now()}.${getExt(file.originalname)}`);
+        }
+    })
+
+    const upload = multer({
+        storage: storage,
+    });
+
+    app.post('/contents/uploadProfileImg/', upload.single('profile'), contents.uploadProfileImg);
 }
