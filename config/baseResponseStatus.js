e21e310678c6841ab0ea521e57c368fe6f74058b
sadad64 <sadad64@naver.com>
2022-08-20 00:04:53 +0900

    [edit] response message edit

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -13,7 +13,7 @@ module.exports = {
     KAKAO_LOGIN_ERROR : { "isSuccess": false, "code": 3500, "message":"카카오 로그인에서 오류가 발생했습니다. 서버로그를 참조해주세요." },
     KAKAO_ACCESS_TOKEN_UNDEFINED : { "isSuccess": false, "code": 3501, "message":"카카오 Access Token 값이 비어있거나 유효하지 않습니다." },
     KAKAO_LOGIN_UNAUTHORIZED_ERROR : { "isSuccess": false, "code": 3502, "message":"카카오 로그인 에러. 인증되지 않은 Token 입니다. (code : 401)" },
-    APPLE_ACCESS_TOKEN_UNDEFINED: { "isSuccess": false, "code": 3503, "message":"Apple Access Token 값이 비어있거나 유효하지 않습니다." },
+    APPLE_ACCESS_TOKEN_UNDEFINED: { "isSuccess": false, "code": 3503, "message":"Apple 인가코드 값이 비어있거나 유효하지 않습니다." },
     APPLE_LOGIN_ERROR :{ "isSuccess": false, "code": 3504, "message":"Apple 로그인에서 오류가 발생했습니다. 서버로그를 참조해주세요." },
 
     // 2000 : 형식적 Validation Error
